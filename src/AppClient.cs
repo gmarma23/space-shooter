5d6fb4062995ae2a6744c7c714b37df1dbe95456
gmarma23 <gmarma99@gmail.com>
2022-12-18 17:35:36 +0200

    Setted AppClient class to handle app execution

---
+++ src/AppClient.cs
@@ -0,0 +1,31 @@
+﻿using SpaceShooter.gui;
+
+namespace SpaceShooter
+{
+    internal static class AppClient
+    {
+        private static MenuFrame menuFrame = new MenuFrame();
+
+        public static void Start() => Application.Run(menuFrame);
+
+        public static void OnMenuOptionPlayClick(object sender, EventArgs e) 
+        {
+            menuFrame.Hide();
+            GameClient.StartGame();
+        }
+
+        public static void OnMenuOptionHighscoresClick(object sender, EventArgs e)
+        {
+            menuFrame.Hide();
+            new HighscoresFrame().Show();
+        }
+
+        public static void OnMenuOptionControlsClick(object sender, EventArgs e)
+        {
+            menuFrame.Hide();
+            new ControlsFrame().Show();
+        }
+
+        public static void OnSubFrameClose(object sender, EventArgs e) => menuFrame.Show();
+    }
+}
