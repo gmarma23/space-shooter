a6b051446b84bffa2dae7ec1a2d213c96525c331
gmarma23 <gmarma99@gmail.com>
2022-12-18 21:46:22 +0200

    Added timers

--- src/GameClient.cs
+++ src/GameClient.cs
@@ -1,22 +1,35 @@
 ﻿using SpaceShooter.core;
 using SpaceShooter.gui;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using Timer = System.Timers.Timer;
 
 namespace SpaceShooter
 {
     internal static class GameClient
     {
-        private static GameFrame gameFrame;
-        private static Gameplay game;
+        private static GameFrame gameFrame = new();
+        private static Gameplay game = new();
+        private static Timer relocateHeroTimer = new();
+        private static Timer relocateEnemiesTimer = new();
+        private static Timer relocateLaserBlastsTimer = new();
+        private static Timer relocateEnemyMissileTimer = new();
+        private static Timer bringEnemyToViewportTimer = new();
+        private static Timer enemyFireLaserTimer = new();
+        private static Timer enemyLaunchMissileTimer = new();
+        private static Timer enemyTeleportTimer = new();
 
         public static void StartGame()
         {
-            gameFrame = new GameFrame();
-            game = new Gameplay();
+            gameFrame = ;
+            game = ;
+            relocateHeroTimer = new Timer();
+            relocateEnemiesTimer = new Timer();
+            relocateLaserBlastsTimer = new Timer();
+            relocateEnemyMissileTimer = new Timer();
+            bringEnemyToViewportTimer = new Timer();
+            enemyFireLaserTimer = new Timer();
+            enemyLaunchMissileTimer = new Timer();
+            enemyTeleportTimer = new Timer();
         }
 
         public static void OnKeyPress(Keys keys)
