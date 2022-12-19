80a93e76efc92a18bceb3e673a51cc49e16fd8c8
gmarma23 <gmarma99@gmail.com>
2022-12-19 17:34:51 +0200

    Added constructor

--- src/core/spaceships/enemy/EnemySpaceship.cs
+++ src/core/spaceships/enemy/EnemySpaceship.cs
@@ -2,8 +2,18 @@
 {
     internal abstract class EnemySpaceship : Spaceship
     {
+        public int Index { get; protected init; }
+        public bool MovesRandomly { get; protected init; }
         public int ScorePoints { get; protected init; }
-        public int RenewDisplacementFrequency { get; protected set; }
+
+        public EnemySpaceship(
+            int hp, int concurrentLaserBlastsCount, int laserBlastDamage, int laserReload, 
+            int missileCount, int missileDamage, int missileReload, bool movesRandomly, int scorePoints) : 
+            base(hp, concurrentLaserBlastsCount, laserBlastDamage, laserReload, missileCount, missileDamage, missileReload)
+        {
+            MovesRandomly = movesRandomly;
+            ScorePoints = scorePoints;
+        }
 
         public abstract void Teleport(int minX, int maxX, int minY, int maxY);
 
