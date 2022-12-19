a11d7a1f78e847bab84afd1ca6653fafdbb005c3
gmarma23 <gmarma99@gmail.com>
2022-12-19 14:44:43 +0200

    Added constructor

--- src/core/spaceships/Spaceship.cs
+++ src/core/spaceships/Spaceship.cs
@@ -1,38 +1,132 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SpaceShooter.core
+﻿namespace SpaceShooter.core
 {
     internal class Spaceship
     {
-        public int TotalHP { get; protected init; }
-        public int AvailableHP { get; protected set; }
         public int XLocation { get; protected set; }
         public int YLocation { get; protected set; }
         public int XDisplacement { get; protected set; }
         public int YDisplacement { get; protected set; }
         public int XVelocity { get; protected set; }
         public int YVelocity { get; protected set; }
-        public int ConcurrentLaserBlastCount { get; protected set; }
-        public int LaserBlastDamage { get; protected set; }
-        public int LaserReload { get; protected set; }
-        public int MissileCount { get; protected set; }
-        public int MissileDamage { get; protected set; }
-        public int MissileReload { get; protected set; }
+        public bool IsDestroyed { get; protected set; }
+
+        public int TotalHP 
+        { 
+            get => TotalHP; 
+            protected init 
+            { 
+                if (value < 0) 
+                    throw new ArgumentException(); 
+            } 
+        }
+
+        public int AvailableHP
+        {
+            get => AvailableHP;
+            protected set
+            {
+                if (value < 0) 
+                    value = 0;
+                else if (value > TotalHP) 
+                    value = TotalHP;
+            }
+        }
+
+        public int ConcurrentLaserBlastCount
+        {
+            get => ConcurrentLaserBlastCount;
+            protected set
+            {
+                if (value < 0)
+                    throw new ArgumentException();
+            }
+        }
+
+        public int LaserBlastDamage
+        {
+            get => LaserBlastDamage;
+            protected set
+            {
+                if (value < 0)
+                    throw new ArgumentException();
+            }
+        }
+
+        public int LaserReload
+        {
+            get => LaserReload;
+            protected set
+            {
+                if (value < 0)
+                    throw new ArgumentException();
+            }
+        }
+
+        public int MissileCount
+        {
+            get => MissileCount;
+            protected set
+            {
+                if (value < 0)
+                    throw new ArgumentException();
+            }
+        }
+
+        public int MissileDamage
+        {
+            get => MissileDamage;
+            protected set
+            {
+                if (value < 0)
+                    throw new ArgumentException();
+            }
+        }
+
+        public int MissileReload
+        {
+            get => MissileReload;
+            protected set
+            {
+                if (value < 0)
+                    throw new ArgumentException();
+            }
+        }
+
+        public Spaceship(
+            int xLocation, int yLocation, 
+            int xDisplacement, int yDisplacement, 
+            int xVelocity, int yVelocity,
+            int hp, int concurrentLaserBlastsCount, int laserBlastDamage, int laserReload, 
+            int missileCount, int missileDamage, int missileReload)
+        {
+            XLocation = xLocation;
+            YLocation = yLocation;
+            XDisplacement = xDisplacement;
+            YDisplacement = yDisplacement;
+            XVelocity = xVelocity;
+            YVelocity = yVelocity;
+            TotalHP = hp;
+            ConcurrentLaserBlastCount = concurrentLaserBlastsCount;
+            LaserBlastDamage = laserBlastDamage;
+            LaserReload = laserReload;
+            MissileCount = missileCount;
+            MissileDamage = missileDamage;
+            MissileReload = missileReload;  
+
+            AvailableHP = TotalHP;
+            IsDestroyed = false;
+        }
 
         public void TakeDamage(int damage) 
         {
             AvailableHP -= damage;
-            if (AvailableHP < 0) AvailableHP = 0;
+            if(AvailableHP == 0) 
+                IsDestroyed = true;
         }
 
         public void RestoreHealth(int health)
         {
             AvailableHP += health;
-            if (AvailableHP > TotalHP) AvailableHP = TotalHP;
         }
     }
 }
