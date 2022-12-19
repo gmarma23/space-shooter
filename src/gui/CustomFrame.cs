aa61546d66c9407ae786eefb7dd69545ba18d4b6
gmarma23 <gmarma99@gmail.com>
2022-12-19 18:30:49 +0200

    Added custom frame for setting universal frame attributes in app

---
+++ src/gui/CustomFrame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace SpaceShooter.src.gui
+{
+    public partial class CustomFrame : Form
+    {
+        public CustomFrame()
+        {
+            InitializeComponent();
+
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Space Shooter";
+            AutoSize = false;
+            MaximizeBox = false;
+            BackColor = Color.Black;
+            Size = new Size(400, 500);
+        }
+    }
+}
