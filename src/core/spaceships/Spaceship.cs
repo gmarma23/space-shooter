1bfa1d30d43f18ef7e99a606b55016ca23ea6e8c
gmarma23 <gmarma99@gmail.com>
2022-12-19 12:37:11 +0200

    Added spaceship properties

--- src/core/spaceships/Spaceship.cs
+++ src/core/spaceships/Spaceship.cs
@@ -8,5 +8,11 @@ namespace SpaceShooter.core
 {
     internal class Spaceship
     {
+        public int TotalHP { get; init; }
+        public int AvailableHP { get; private set; }
+        public int LaserBlastCount { get; private set; }
+        public int LaserBlastDamage { get; private set; }
+        public int XDisplacement { get; private set; }
+        public int YDisplacement { get; private set; }
     }
 }
