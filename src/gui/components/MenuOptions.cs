a9d3a671f9f144150bd22c99ec31826faab0369b
gmarma23 <gmarma99@gmail.com>
2022-12-19 19:11:17 +0200

    Added menu components

---
+++ src/gui/components/MenuOptions.cs
@@ -0,0 +1,18 @@
+﻿using SpaceShooter.gui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceShooter.gui
+{
+    internal class MenuOptions : Panel
+    {
+        private MenuButton playBtn;
+        private MenuButton highscoresBtn;
+        private MenuButton controlsBtn;
+
+
+    }
+}
