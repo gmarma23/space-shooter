e7e36a52b278137d5cb2d4c083e40326ddf204ad
gmarma23 <gmarma99@gmail.com>
2022-12-20 17:12:19 +0200

    Implemented custom menu button

--- src/gui/components/MenuButton.cs
+++ src/gui/components/MenuButton.cs
@@ -1,13 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing.Drawing2D;
 
 namespace SpaceShooter.gui
 {
     internal class MenuButton : Button
     {
+        public MenuButton(MenuOptions menuOptionsPanel, string text, EventHandler onClick) 
+        {
+            BackColor = Color.FromArgb(80, Color.White);
+            FlatAppearance.BorderSize = 0;
+            FlatStyle = FlatStyle.Flat;
+            Font = new Font("Arial", 12.0F, FontStyle.Regular, GraphicsUnit.Point);
+            ForeColor = Color.White;
+            Parent = menuOptionsPanel;
+            Width = Parent.Width;
+            Height = menuOptionsPanel.MenuButtonHeight;
+            UseVisualStyleBackColor = false;
+            Text = text;
+            addEventHandlers(onClick);
+        }
 
+        private void addEventHandlers(EventHandler onClick)
+        {
+            Click += onClick;
+            MouseEnter += onMouseEnter;
+            MouseLeave += onMouseLeave;
+        }
+
+        private void onMouseEnter(object sender, EventArgs e)
+        {
+            UseVisualStyleBackColor = false;
+            FlatAppearance.MouseOverBackColor = Color.FromArgb(120, Color.White);
+        }
+
+        private void onMouseLeave(object sender, EventArgs e)
+        {
+            UseVisualStyleBackColor = true;
+            BackColor = Color.FromArgb(80, Color.White);
+        }
     }
 }
