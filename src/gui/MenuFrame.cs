239da39351781821fc8e5328a185f0057a14a1f8
gmarma23 <gmarma99@gmail.com>
2022-12-20 17:13:26 +0200

    Added menu options to frame

--- src/gui/MenuFrame.cs
+++ src/gui/MenuFrame.cs
@@ -1,24 +1,20 @@
-﻿using SpaceShooter.src.gui;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using SpaceShooter.gui;
 
 namespace SpaceShooter.gui
 {
     public partial class MenuFrame : CustomFrame
     {
+        public const double optionsYLocationRatio = 0.5;
+        public const double optionsFrameWidthRatio = 0.35;
+        public const double optionsFrameHeightRatio = 0.3;
+
         private Label title;
         private MenuOptions menuOptions;
-
+        
         public MenuFrame()
         {
             InitializeComponent();
+            menuOptions = new MenuOptions(this);
         }
     }
 }
