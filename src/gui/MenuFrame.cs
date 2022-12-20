bac87d2b56efea6fd55c7bc2b33b41214e1b27ac
gmarma23 <gmarma99@gmail.com>
2022-12-20 21:59:54 +0200

    Added and setted title label

--- src/gui/MenuFrame.cs
+++ src/gui/MenuFrame.cs
@@ -4,9 +4,13 @@ namespace SpaceShooter.gui
 {
     public partial class MenuFrame : CustomFrame
     {
-        public const double optionsYLocationRatio = 0.5;
-        public const double optionsFrameWidthRatio = 0.35;
-        public const double optionsFrameHeightRatio = 0.3;
+        public const double titleWidthRatio = 0.8;
+        public const double titleHeightRatio = 0.3;
+        public const double titleYLocationRatio = 0.02;
+
+        public const double optionsYLocationRatio = 0.77;
+        public const double optionsWidthRatio = 0.35;
+        public const double optionsHeightRatio = 0.3;
 
         private Label title;
         private MenuOptions menuOptions;
@@ -14,7 +18,30 @@ namespace SpaceShooter.gui
         public MenuFrame()
         {
             InitializeComponent();
+
             menuOptions = new MenuOptions(this);
+
+            title = new Label();
+            setTitleLabel();
+        }
+
+        private void setTitleLabel()
+        {
+            Controls.Add(title);
+            title.BackColor = Color.Transparent;
+            title.Font = new Font(
+                "Microsoft Sans Serif", 
+                25.8F, FontStyle.Bold | FontStyle.Italic, 
+                GraphicsUnit.Point);
+            title.ForeColor = Color.White;
+            title.Width = (int)(titleWidthRatio * ClientRectangle.Width);
+            title.Height = (int)(titleHeightRatio * ClientRectangle.Height);
+            title.Text = "SPACE SHOOTER";
+            title.TextAlign = ContentAlignment.MiddleCenter;
+            title.UseCompatibleTextRendering = true;
+            title.Location = new Point(
+                ClientRectangle.Width / 2 - title.Width / 2,
+                (int)(ClientRectangle.Height * titleYLocationRatio));
         }
     }
 }
