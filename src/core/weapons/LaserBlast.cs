8abc97c5525a4e89306bad950bca5030cda8aac3
gmarma23 <gmarma99@gmail.com>
2022-12-21 17:53:36 +0200

    Provided properties, constructor and method for movement

--- src/core/weapons/LaserBlast.cs
+++ src/core/weapons/LaserBlast.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SpaceShooter.core
+﻿namespace SpaceShooter.core
 {
     internal class LaserBlast
     {
+        public bool IsEnemy { get; private init; }
+        public int Damage { get; private init; }
+        public int XLocation { get; private init; }
+        public int Width { get; private init; }
+        public int Height { get; private init; }
+        public int YDisplacement
+        {
+            get => YDisplacement;
+            private init
+            {
+                if (value < 0)
+                    throw new ArgumentException();
+                _ = value;
+            }
+        }
+        public int YLocation { get; private set; }
+
+        public LaserBlast(bool isEnemy, int damage, int displacement, int initXLocation, int initYLocation, int width, int height)
+        {
+            IsEnemy = isEnemy;
+            Damage = damage;
+            YDisplacement = displacement;
+            XLocation = initXLocation;
+            YLocation = initYLocation;
+            Width = width;
+            Height = height;
+        } 
+
+        public void MoveVertically()
+        {
+            YLocation = (IsEnemy ? 1 : -1) * YDisplacement; 
+        }
     }
 }
