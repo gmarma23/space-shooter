6c9cf9250dc744a6181449dfc29dc74a972b22b4
gmarma23 <gmarma99@gmail.com>
2022-12-21 17:29:40 +0200

    Added constructor and method to update displacement

--- src/core/spaceships/HeroSpaceship.cs
+++ src/core/spaceships/HeroSpaceship.cs
@@ -1,18 +1,42 @@
-﻿namespace SpaceShooter.core
+﻿using System.Diagnostics;
+
+namespace SpaceShooter.core
 {
     internal class HeroSpaceship : Spaceship
     {
+        private readonly int defaultDisplacement;
+
         public bool GoUp { get; set; }
         public bool GoDown { get; set; }
         public bool GoLeft { get; set; }
         public bool GoRight { get; set; }
 
-        public HeroSpaceship(
-            int hp, int concurrentLaserBlastsCount, int laserBlastDamage, int laserReload,
-            int missileCount, int missileDamage, int missileReload, bool movesRandomly, int scorePoints) :
-            base(hp, concurrentLaserBlastsCount, laserBlastDamage, laserReload, missileCount, missileDamage, missileReload)
+        public HeroSpaceship(int defaultDisplacement, int initXLocation, int initYLocation, int width, int height, int hp,
+            int concurrentLaserBlastsCount, int laserBlastDamage, int laserReload,
+            int missileCount, int missileDamage, int missileReload) :
+            base (false, initXLocation, initYLocation, width, height, hp, 
+                concurrentLaserBlastsCount, laserBlastDamage, laserReload, 
+                missileCount, missileDamage, missileReload)
         {
+            this.defaultDisplacement = defaultDisplacement;
 
+            GoLeft = false;
+            GoRight = false;
+            GoUp = false;
+            GoDown = false;
+
+            UpdateDisplacement();
+        }
+
+        public void UpdateDisplacement()
+        {
+            XDisplacement = 0;
+            YDisplacement = 0;
+
+            if (GoLeft) XDisplacement -= defaultDisplacement;
+            if (GoRight) XDisplacement += defaultDisplacement;
+            if (GoUp) YDisplacement -= defaultDisplacement;
+            if (GoDown) YDisplacement += defaultDisplacement;
         }
     }
 }
