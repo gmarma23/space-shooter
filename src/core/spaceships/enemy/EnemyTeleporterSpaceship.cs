60faaf99ae5d427639afd6671a79972178dcc408
gmarma23 <gmarma99@gmail.com>
2022-12-23 14:10:56 +0200

    Call property set related methods in constructor

--- src/core/spaceships/enemy/EnemyTeleporterSpaceship.cs
+++ src/core/spaceships/enemy/EnemyTeleporterSpaceship.cs
@@ -5,10 +5,12 @@
         public EnemyTeleporterSpaceship(GameGrid grid, bool randomMotion, int absMaxDisplacement, int hp,
             int concurrentLaserBlastsCount, int laserBlastDamage, int laserReloadTime,
             int missileCount, int missileDamage, int missileReloadTime, int scorePoints) :
-            base(grid, randomMotion, hp, absMaxDisplacement, concurrentLaserBlastsCount, laserBlastDamage, laserReloadTime,
+            base(randomMotion, hp, absMaxDisplacement, concurrentLaserBlastsCount, laserBlastDamage, laserReloadTime,
                 missileCount, missileDamage, missileReloadTime, scorePoints)
         {
-
+            setSize(grid);
+            setInitXLocation(grid);
+            setInitYLocation(grid);
         }
 
         public override void Teleport(int minX, int maxX, int minY, int maxY)
