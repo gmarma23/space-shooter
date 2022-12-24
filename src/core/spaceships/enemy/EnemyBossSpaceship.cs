3a89cbdca98e358d7985c4c5bd24af0cc29de741
gmarma23 <gmarma99@gmail.com>
2022-12-24 17:39:28 +0200

    Setting properties in constructor

--- src/core/spaceships/enemy/EnemyBossSpaceship.cs
+++ src/core/spaceships/enemy/EnemyBossSpaceship.cs
@@ -41,9 +41,12 @@ namespace SpaceShooter.core
         public EnemyBossSpaceship(GameGrid grid, bool randomMotion = false, int absMaxDisplacement = 5, int hp = 1000,
             int concurrentLaserBlastsCount = 2, int laserBlastDamage = 50, int laserReloadTime = 1,
             int missileCount = 3, int missileDamage = 100, int missileReloadTime = 10, int scorePoints = 5) :
-            base(randomMotion, hp, absMaxDisplacement, concurrentLaserBlastsCount, laserBlastDamage, laserReloadTime,
-                missileCount, missileDamage, missileReloadTime, scorePoints)
+            base(randomMotion, hp, absMaxDisplacement, concurrentLaserBlastsCount, laserBlastDamage, laserReloadTime, scorePoints)
         {
+            MissileCount = missileCount;
+            MissileDamage = missileDamage;
+            MissileReloadTime = missileReloadTime;
+
             setSize(grid, bossSpaceshipScaleFactor);
             setGridLimits(grid);
             initializeLocationX();
