2886413fb932555cc53d1ccac2d36b243e12a9c4
gmarma23 <gmarma99@gmail.com>
2022-12-24 16:40:07 +0200

    Implemented ITeleport interface method

--- src/core/spaceships/enemy/EnemyTeleporterSpaceship.cs
+++ src/core/spaceships/enemy/EnemyTeleporterSpaceship.cs
@@ -1,22 +1,26 @@
 ﻿namespace SpaceShooter.core
 {
-    internal class EnemyTeleporterSpaceship : EnemySpaceship
+    internal class EnemyTeleporterSpaceship : EnemySpaceship, ITeleport
     {
+        protected Random random;
+
         public EnemyTeleporterSpaceship(GameGrid grid, bool randomMotion = true, int absMaxDisplacement = 7, int hp = 400,
             int concurrentLaserBlastsCount = 1, int laserBlastDamage = 30, int laserReloadTime = 1500,
             int missileCount = 0, int missileDamage = 0, int missileReloadTime = 0, int scorePoints = 2) :
             base(randomMotion, hp, absMaxDisplacement, concurrentLaserBlastsCount, laserBlastDamage, laserReloadTime,
                 missileCount, missileDamage, missileReloadTime, scorePoints)
         {
+            random = new Random();
             setSize(grid);
             setGridLimits(grid);
             initializeLocationX();
             initializeLocationY();
         }
 
-        public override void Teleport(int minX, int maxX, int minY, int maxY)
+        public void Teleport()
         {
-
+            LocationX = generateRandomX();
+            LocationY = generateRandomY();
         }
     }
 }
