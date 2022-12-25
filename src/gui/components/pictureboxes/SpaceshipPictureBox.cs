68f343c39c0f8356f9b74455d075190395773e9e
gmarma23 <gmarma99@gmail.com>
2022-12-25 11:43:02 +0200

    Implemented spaceship picture box gui component

--- src/gui/components/pictureboxes/SpaceshipPictureBox.cs
+++ src/gui/components/pictureboxes/SpaceshipPictureBox.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SpaceShooter.gui
+﻿namespace SpaceShooter.gui
 {
     internal class SpaceshipPictureBox : PictureBox
     {
+        private static readonly Dictionary<SpaceshipType, Bitmap> spaceshipImages = new Dictionary<SpaceshipType, Bitmap>() 
+        { 
+            {SpaceshipType.Hero, resources.Resources.hero_spaceship },
+            {SpaceshipType.EnemyFighter, resources.Resources.enemy_fighter_spaceship },
+            {SpaceshipType.EnemyTeleporter, resources.Resources.enemy_teleporter_spaceship },
+            {SpaceshipType.EnemyBoss, resources.Resources.enemy_boss_spaceship }
+        };
+
+        public SpaceshipPictureBox(SpaceshipType type, int width, int height) 
+        {
+            Width = width;
+            Height = height;
+            Image = spaceshipImages[type];
+        }
+
+        public enum SpaceshipType
+        {
+            Hero,
+            EnemyFighter,
+            EnemyTeleporter,
+            EnemyBoss
+        }
     }
 }
