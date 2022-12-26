86617e5af90ec51dcc472dc9ae98ea498a9f80a5
gmarma23 <gmarma99@gmail.com>
2022-12-26 13:43:15 +0200

    Added interface for GameState methods used by Gui to draw state

---
+++ src/core/IDrawGameStateUI.cs
@@ -0,0 +1,17 @@
+﻿namespace SpaceShooter.core
+{
+    internal interface IDrawGameStateUI
+    {
+        public (int, int) GetSpaceshipSize(bool isHero);
+
+        public (int, int) GetSpaceshipLocation(bool isHero);
+
+        public EnemySpaceshipType GetEnemySpaceshipType();
+
+        public (int, int) GetLaserBlastSize(int numCode);
+
+        public (int, int) GetLaserBlastLocation(int numCode);
+
+        public bool IsHeroLaserBlast(int numCode);
+    }
+}
