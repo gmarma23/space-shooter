aedb4c9a031895992fafa49d621f1b677b688015
gmarma23 <gmarma99@gmail.com>
2022-12-31 12:27:24 +0200

    Added explosion image

--- resources/Resources.Designer.cs
+++ resources/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace SpaceShooter.resources {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap explosion {
+            get {
+                object obj = ResourceManager.GetObject("explosion", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap hero_spaceship {
             get {
                 object obj = ResourceManager.GetObject("hero_spaceship", resourceCulture);
