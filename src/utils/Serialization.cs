b5fb3f09dafb43cf06b21b7da77945d194e33028
gmarma23 <gmarma99@gmail.com>
2023-01-01 14:12:46 +0200

    Implemented serialization methods

--- src/utils/Serialization.cs
+++ src/utils/Serialization.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
 
 namespace SpaceShooter.src.utils
 {
-    internal class Serialization
+    public static class Serialization
     {
+        public static void Serialize(object obj, string filename)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.Write);
+            formatter.Serialize(stream, obj);
+            stream.Close();
+        }
+
+        public static T Deserialize<T>(string filename)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            T obj = (T)formatter.Deserialize(stream);
+            stream.Close();
+            return obj;
+        }
     }
 }
