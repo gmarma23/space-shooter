0c94e572381ed1aca55c6252c049564d70fda5dd
gmarma23 <gmarma99@gmail.com>
2023-03-22 11:51:23 +0200

    Added draft implementation of Highscore items

---
+++ src/gui/components/HighscoreLabel.cs
@@ -0,0 +1,21 @@
+﻿namespace SpaceShooter.gui
+{
+    public class HighscoreLabel : Label
+    {
+        public HighscoreLabel(Control parent)
+        {
+            Parent = parent;
+            Parent.Controls.Add(this);
+            BackColor = Color.Transparent;
+            AutoSize = true;
+            Font = new Font(
+                "Microsoft Sans Serif",
+                16.0F, GraphicsUnit.Point
+            );
+            ForeColor = Color.White;
+            TextAlign = ContentAlignment.MiddleCenter;
+            UseCompatibleTextRendering = true;
+            Left = Parent.ClientRectangle.Width / 2 - Width / 2;
+        }
+    }
+}
