7a4fbf54bca79d49322c07065e524ed1b33e869c
gmarma23 <gmarma99@gmail.com>
2023-03-25 23:05:15 +0200

    Added button click soundFx

--- src/gui/components/menu/MenuButton.cs
+++ src/gui/components/menu/MenuButton.cs
@@ -1,7 +1,12 @@
-﻿namespace SpaceShooter.gui
+﻿using SpaceShooter.resources;
+using SpaceShooter.utils;
+
+namespace SpaceShooter.gui
 {
-    internal class MenuButton : Button
+    public class MenuButton : Button
     {
+        private static readonly CachedSound menuButtonClickSoundFx = new(Resources.aud_menu_click);
+
         public MenuButton(Control parent) 
         {
             Parent = parent;
@@ -17,18 +22,22 @@
 
             MouseEnter += onMouseEnter;
             MouseLeave += onMouseLeave;
+            Click += onMouseClick;
         }
 
-        private void onMouseEnter(object sender, EventArgs e)
+        private void onMouseEnter(object? sender, EventArgs e)
         {
             UseVisualStyleBackColor = false;
             FlatAppearance.MouseOverBackColor = Color.FromArgb(120, Color.White);
         }
 
-        private void onMouseLeave(object sender, EventArgs e)
+        private void onMouseLeave(object? sender, EventArgs e)
         {
             UseVisualStyleBackColor = true;
             BackColor = Color.FromArgb(60, Color.White);
         }
+
+        private void onMouseClick(object? sender, EventArgs e) 
+            => AudioPlayer.Player.PlaySound(menuButtonClickSoundFx);
     }
 }
