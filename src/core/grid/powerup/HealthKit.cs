468add0f6f7e0bd8f747db0de52a77a66844fa8f
gmarma23 <gmarma99@gmail.com>
2023-03-26 02:27:17 +0200

    Introduced health kit powerup

---
+++ src/core/grid/powerup/HealthKit.cs
@@ -0,0 +1,36 @@
+﻿using SpaceShooter.resources;
+using SpaceShooter.utils;
+
+namespace SpaceShooter.core
+{
+    public class HealthKit : PowerUp
+    {
+        protected static readonly CachedSound healthBoostSoundFx = new(Resources.aud_health_boost);
+        protected int health;
+
+        public HealthKit(int health = 50)
+        {
+            this.health = health;
+            Image = Resources.img_health_kit;
+        }
+
+        protected override void checkTargetReceived()
+        {
+            if (Target == null)
+                return;
+
+            if (!Target.IsActive)
+            {
+                Target = null;
+                return;
+            }
+
+            if (GameGrid.ItemsIntersect(this, Target))
+            {
+                AudioPlayer.Player.PlaySound(healthBoostSoundFx);
+                Target.RestoreHealth(health);
+                IsActive = false;
+            }
+        }
+    }
+}
---
+++ src/core/grid/powerup/PowerUp.cs
@@ -0,0 +1,41 @@
+﻿namespace SpaceShooter.core
+{
+    public abstract class PowerUp : GridItem
+    {
+        private static int id = 0;
+
+        public IHPGridItem? Target { get; set; }
+        public int ID { get; protected init; }
+
+        public PowerUp()
+        {
+            Target = null;
+            IsActive = true;
+            ID = id++;
+        }
+
+        public override void Move()
+        {
+            if (!IsActive)
+                return;
+
+            moveVertically();
+            checkTargetReceived();
+        }
+        
+        protected abstract void checkTargetReceived();
+
+        protected override void moveVertically()
+            => LocationY += DeltaTimeDisplacementY;
+
+        protected override void moveHorizontally() { }
+
+        protected override void setBounds(GameGrid grid)
+        {
+            minX = grid.GetItemMinPossibleX();
+            maxX = grid.GetItemMaxPossibleX(this);
+            minY = grid.GetItemMinPossibleY();
+            maxY = grid.GetItemMaxPossibleY(this);
+        }
+    }
+}
