a7a5d2d0b5752f8e8a72ba9d8b63686425487a46
gmarma23 <gmarma99@gmail.com>
2023-04-03 09:57:13 +0300

    Implemented TextLabel

---
+++ src/gui/TextLabel.cs
@@ -0,0 +1,14 @@
+﻿namespace SpaceShooter.gui
+{
+    public class TextLabel : CustomLabel
+    {
+        private const float parentHeightRatio = 0.3f;
+        private const float parentWidthRatio = 0.8f;
+
+        public TextLabel(Control parent, string text)
+            : base(parent, text, parentHeightRatio, parentWidthRatio)
+        {
+            TextAlign = ContentAlignment.MiddleLeft;
+        }
+    }
+}
