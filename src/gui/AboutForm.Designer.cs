a47a3b1758ea33c508541eda8cc737e9e582ec23
gmarma23 <gmarma99@gmail.com>
2023-04-03 09:57:44 +0300

    Populated About form

--- src/gui/AboutForm.Designer.cs
+++ src/gui/AboutForm.Designer.cs
@@ -34,7 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(443, 516);
+            ClientSize = new Size(443, 431);
             Name = "AboutForm";
             ResumeLayout(false);
         }
--- src/gui/AboutForm.cs
+++ src/gui/AboutForm.cs
@@ -1,13 +1,32 @@
-﻿namespace SpaceShooter.gui
+﻿using System.Collections.Generic;
+
+namespace SpaceShooter.gui
 {
     public partial class AboutForm : CustomForm
     {
+        const float textLabelMarginRatio = 0.1f;
+
         public AboutForm()
         {
             InitializeComponent();
             setBackgroundImage();
 
-            new FormTitleLabel(this, "ABOUT");
+            var titleLabel = new FormTitleLabel(this, "    ABOUT    ");
+
+            int textLabelMargin = (int)(Height * textLabelMarginRatio);
+
+            string gameDescription = " Exterminate hostile spaceships and\n survive as many waves as possible,\n aiming for the top of the highscores\n list.";
+            var gameDescriptionLabel = new TextLabel(this, gameDescription)
+            {
+                Top = titleLabel.Top + titleLabel.Height + textLabelMargin
+            };
+
+            string controls = " Use arrow keys or WASD keys to move\n hero and space bar to fire laser blaster.";
+
+            var controlsLabel = new TextLabel(this, controls)
+            {
+                Top = gameDescriptionLabel.Top + gameDescriptionLabel.Height + textLabelMargin
+            };
         }
     }
 }
