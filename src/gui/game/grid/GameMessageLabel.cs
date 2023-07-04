01f77c687a8d1e15baf536233fc478ef6825d97b
gmarma23 <gmarma99@gmail.com>
2023-07-04 16:55:44 +0300

    Generalized message label

--- src/gui/game/grid/GameMessageLabel.cs
+++ src/gui/game/grid/GameMessageLabel.cs
@@ -1,14 +1,14 @@
 ﻿namespace SpaceShooter.gui
 {
-    public class GameOverLabel : CustomLabel
+    public class GameMessageLabel : CustomLabel
     {
         private const float parentHeightRatio = 0.15f;
         private const float parentWidthRatio = 0.5f;
 
-        public GameOverLabel(Control parent, string text)
+        public GameMessageLabel(Control parent, string text)
             : base(parent, text, parentHeightRatio, parentWidthRatio, FontStyle.Bold | FontStyle.Italic)
-        {
-            ForeColor = Color.DarkRed;
+        { 
+
         }
     }
 }
